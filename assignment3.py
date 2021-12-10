3764933bea031cc5f28a5b29619e678c858f7426
Ubuntu <lzlniu@BDMA3.u4dbx2fi4eku3knu41zihdl0gf.fx.internal.cloudapp.net>
2021-12-10 23:11:56 +0000

    update mlflow

--- assignment3.py
+++ assignment3.py
@@ -1,39 +1,23 @@
-## Getting the data
-
-from influxdb import InfluxDBClient # install via "pip install influxdb "
 import pandas as pd
 import numpy as np
+import sys
+import mlflow
+from azureml.core import Workspace
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.model_selection import TimeSeriesSplit, train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error,r2_score
+from sklearn.base import BaseEstimator, TransformerMixin
 
-client = InfluxDBClient(host='influxus.itu.dk', port=8086 , username='lsda', password='icanonlyread')
-client.switch_database('orkney')
+num_of_splits = int(sys.argv[1]) if len(sys.argv) > 1 else 5
+num_of_drgree = int(sys.argv[2]) if len(sys.argv) > 2 else 2
 
-def get_df(results):
-    values = results.raw["series"][0]["values"]
-    columns = results.raw["series"][0]["columns"]
-    df = pd.DataFrame(values, columns = columns).set_index("time")
-    df.index = pd.to_datetime(df.index) # Convert to datetime - index
-    return df
-
-# Get the power generation data from 2020-10-01 to 2021-10-01 and calculate its average for each 3 hour time period.
-generation = client.query("SELECT time,mean(Total) FROM Generation where time > '2020-10-01' and time <= '2021-10-01' GROUP BY time(3h)")
-
-# Get the weather forecasts from 2020-10-01 to 2021-10-01 with the shortest lead time (Lead hours is 1)
-wind = client.query("SELECT time,Direction,Speed FROM MetForecasts where time > '2020-10-01' and time <= '2021-10-01' and Lead_hours = '1'") # earlist time '2019-03-29'
-
-gen_df = get_df(generation)
-wind_df = get_df(wind)
-
-print('Original gen_df data num:',len(gen_df))
-print('Original wind_df data num:',len(wind_df))
-if (len(gen_df)>len(wind_df)): df = gen_df.join(wind_df) # left join, gen_df
-else: df = wind_df.join(gen_df)
-print('Combined(joined) data num:',len(df))
-
-# df = df[df['mean'].notnull()] # drop the column without mean 
-# print('Removed data with power generation column is null, remained data num:',len(df))
-
-# df = df[df['Speed'].notnull()]
-# print('removed data with wind speed column is null, remained data num:',len(df))
+discrete_props = ['Direction'] # demonstrate which column of data is discrete feature (indicating others are linear) 
 
 def fillnan(df, col_name):
     num=0
@@ -49,22 +33,6 @@ def fillnan(df, col_name):
             #print(num,':',df[col_name][num],'\t',num+1,':',df[col_name][num+1])
         num+=1
     return df
-
-# Fill all nan
-df=fillnan(df,'mean')
-df=fillnan(df,'Direction')
-df=fillnan(df,'Speed')
-
-## Preprocess the data
-
-from sklearn import preprocessing
-from sklearn.pipeline import Pipeline
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
-from sklearn.svm import SVR
-from sklearn.base import BaseEstimator, TransformerMixin
-
-discrete_props = ['Direction'] # demonstrate which column of data is discrete feature (indicating others are linear) 
 
 class HourAdder(BaseEstimator,TransformerMixin):
     def fit(self, X, y):
@@ -100,35 +68,67 @@ class WindDirectCoder():
 #         for i in X.columns: fillnan(X,i)
 #         return X
 
-pipeline = Pipeline([
-    #("FillNaN", FillNaN()), # fill the row which its column without Speed or Direction
-    #("HourAdder", HourAdder()), # commit it because bad performance
-    ("WindDirectCoder", WindDirectCoder()), # add this Direction feature is good for the model in general
-    #("LinReg", LinearRegression()), # use Linear Regression model
-    #("RFReg", RandomForestRegressor()), # use Random Forest model
-    ("SVR", SVR(gamma=0.1, C=20.0)), # use Support Vector Machine model, which have the best performance
-])
+# Preprocess the data
 
-# Fit the pipeline
-from sklearn.model_selection import train_test_split
+# Use Microsoft Azure Machine Learning Studio for experiment tracking. (pip install azureml-core)
+#ws = Workspace.from_config()
+#mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
+#mlflow.set_tracking_uri("azureml://northeurope.api.azureml.ms/mlflow/v1.0/subscriptions/b33cbdfb-bc26-490c-af0b-0518d16378fc/resourceGroups/ITU_BDM_ResourceGroup/providers/Microsoft.MachineLearningServices/workspaces/BDMA3ML")
+#mlflow.set_experiment("zeli - power generation prediction experiment")
 
-x_train, x_test, y_train, y_test = train_test_split(pd.DataFrame(df,columns=['Direction','Speed']), df['mean'], test_size=0.2, random_state=42)
-pipeline.fit(x_train,y_train)
-y_hat = pipeline.predict(x_test)
+# Start a run
+with mlflow.start_run(run_name="splits5degree2"):
+    # Load data
+    df = pd.read_json("/home/lzlniu/ITU_big_data_course/dataset.json", orient="split")
+    # Handle missing data (Fill nan with fillnan function)
+    df=fillnan(df,'Total')
+    df=fillnan(df,'Direction')
+    df=fillnan(df,'Speed')
+    pipeline = Pipeline([
+        #("FillNaN", FillNaN()), # fill the row which its column without Speed or Direction
+        #("HourAdder", HourAdder()), # commit it because bad performance
+        ("WindDirectCoder", WindDirectCoder()), # add this Direction feature is good for the model in general
+        ("Poly", PolynomialFeatures(degree = num_of_degree)), # add polynomial features, num_of_degree is on the top, default 2
+        ("LinReg", LinearRegression()), # use Linear Regression model
+        #("RFReg", RandomForestRegressor()), # use Random Forest model
+        #("SVR", SVR(gamma=0.1, C=20.0)), # use Support Vector Machine model, which have the best performance
+    ])
+    metrics = [
+            ("MAE", mean_absolute_error, []),
+        ]
+    X = df[["Speed","Direction"]]
+    y = df["Total"]
+    number_of_splits = num_of_splits # on the top, default 5
+    mlflow.log_param('splits', num_of_splits)
+    mlflow.log_param('degree', num_of_degree)
+    mlflow.log_param('model', pipeline.steps[-1][0])
+    # Fit the pipeline
+    for train, test in TimeSeriesSplit(number_of_splits).split(X,y):
+        pipeline.fit(X.iloc[train],y.iloc[train])
+        predictions = pipeline.predict(X.iloc[test])
+        truth = y.iloc[test]
+        plt.plot(truth.index, truth.values, label="Truth")
+        plt.plot(truth.index, predictions , label ="Predictions")
+        plt.show()
+        # Calculate and save the metrics for this fold
+        for name, func, scores in metrics:
+            score = func(truth, predictions)
+            scores.append(score)
+    # Log a summary of the metrics
+    for name, _, scores in metrics:
+        # Note: Here we just log the mean of the scores.
+        # Are there other summarizations that could be interesting?
+        mean_score = sum(scores)/number_of_splits
+        mlflow.log_metric(f"mean_{name}", mean_score)
 
-from sklearn.metrics import mean_absolute_error
-mae = mean_absolute_error(y_test, y_hat)
-
-print("Training MAE:", mae)
-
-## Do forecasting
+# Do forecasting
 # Get all future forecasts regardless of lead time
-forecasts = client.query("SELECT * FROM MetForecasts where time > now()")
-for_df = get_df(forecasts)
+#forecasts = client.query("SELECT * FROM MetForecasts where time > now()")
+#for_df = get_df(forecasts)
 # Limit to only the newest source time
-newest_source_time = for_df["Source_time"].max()
-newest_forecasts = for_df.loc[for_df["Source_time"] == newest_source_time].copy()
+#newest_source_time = for_df["Source_time"].max()
+#newest_forecasts = for_df.loc[for_df["Source_time"] == newest_source_time].copy()
 # only use the Direction and Speed as input X
-newest_forecasts = pd.DataFrame(newest_forecasts,columns=['Direction','Speed'])
+#newest_forecasts = pd.DataFrame(newest_forecasts,columns=['Direction','Speed'])
 # Preprocess the forecasts and do predictions in one fell swoop
-pipeline.predict(newest_forecasts)
+#pipeline.predict(newest_forecasts)
